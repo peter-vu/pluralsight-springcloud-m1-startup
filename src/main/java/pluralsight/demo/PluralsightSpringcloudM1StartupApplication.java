02fc44d5bfbb5d509a4d1d60af2a4c728557d746
Peter Vu <vutuananh.vn.sg@gmail.com>
2021-04-13 23:33:09 +0800

    First commit

---
+++ src/main/java/pluralsight/demo/PluralsightSpringcloudM1StartupApplication.java
@@ -0,0 +1,13 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM1StartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM1StartupApplication.class, args);
+	}
+
+}
